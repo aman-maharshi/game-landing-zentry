d56ace3b02d9293bc3f7262669a84916a693f7dc
aman-maharshi <amanm4harshi@gmail.com>
2024-12-13 22:45:01 +0530

    fonts and colors setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <main>
       <h1 className='text-4xl font-bold'>Welcome to Awwards</h1>
+      <p className='text-violet-300'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, consequuntur!</p>
     </main>
   )
 }
